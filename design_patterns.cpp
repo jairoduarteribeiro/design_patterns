1f4f7902e149404ba1a36108265f6fac28e9dc0e
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-05-28 16:37:07 +0100

    create project

---
+++ design_patterns.cpp
@@ -0,0 +1,12 @@
+﻿// design_patterns.cpp : Defines the entry point for the application.
+//
+
+#include "design_patterns.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
